28268c09d63036bd2a5c6d3c104d5499c78f0af4
Jay Liu <jliu2589@gmail.com>
2023-02-14 18:11:02 -0500

    App Skeleton

---
+++ src/components/Accounts.tsx
@@ -0,0 +1,4 @@
+function Accounts() {
+  return <div>This is where I list all my accounties</div>;
+}
+export default Accounts;
---
+++ src/components/Chart.tsx
@@ -0,0 +1,4 @@
+function Chart() {
+  return <div>This is where the charty goes</div>;
+}
+export default Chart;
